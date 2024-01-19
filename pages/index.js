6ba3ec6e2a38aac09c948797cbc3c31d1dc12bb1
Yoonyesol <maniac970430@naver.com>
2024-01-20 00:25:43 +0900

    🔨 chore: favicon 추가 및 SEO를 위한 메타데이터 설정

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,21 @@ import Head from "next/head";
 const Home = () => {
   return (
     <div>
-      <Head></Head>
+      <Head>
+        <title>심플 룰렛</title>
+        <meta
+          name="description"
+          content="흥미로운 룰렛 게임을 간단히 플레이할 수 있는 사이트입니다. 결정이 필요한 다양한 상황에서 룰렛 당첨 결과를 유용하게 사용해 보세요!"
+        />
+        <meta
+          name="keywords"
+          content="룰렛, 네이버 룰렛, 게임, 온라인, 재미, 선택장애, 흥미, 당첨"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="룰렛 게임사" />
+        <meta name="email" content="seaofiandme@google.com" />
+        <meta property="og:type" content="website" />
+      </Head>
       <Roulette />
     </div>
   );
