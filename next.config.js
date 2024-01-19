a578a9b67e8bff84db6b5aad5dbfc06f03fc6ac0
Yoonyesol <maniac970430@naver.com>
2024-01-19 22:38:41 +0900

    🐛 fix: 배포 시 css 적용되지 않는 버그 수정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "http://Yoonyesol.github.io/roulette"
+      : "",
 };
 
 module.exports = nextConfig;
