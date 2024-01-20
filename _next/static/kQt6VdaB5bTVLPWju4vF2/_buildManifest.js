817b49a70a1393672b52cb069080ea0efef0fd8c
Yoonyesol <maniac970430@naver.com>
2024-01-20 21:57:28 +0900

    Updates

---
+++ _next/static/kQt6VdaB5bTVLPWju4vF2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a00fb9a725135eb9.css","static/chunks/pages/index-aa88dce210a4b333.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
